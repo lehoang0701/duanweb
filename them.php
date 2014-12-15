cdf11ebc3a22e09f82f45885a972d2e56eaa6da0
CuongDao <dqcuong93@yahoo.com.vn>
2014-12-15 11:58:38 +0700

    thêm chức năng xóa
    Võ Xuân An - version 1.3

--- them.php
+++ them.php
@@ -54,6 +54,63 @@ mysqli_query($sqli,"SET NAMES 'UTF8'");
 </form>
 
 
+// Võ Xuân An - version 1.3 - chức năng xóa bài viết 
+<?php
+    $sqli = new mysqli(SERVER, USER, PASS, DB);
+    mysqli_query($sqli,"SET NAMES 'UTF8'");
+    ?>
+
+<form action="module/chuyenmuc/xuly.php" method="post">
+<div class="left">
+<table width="423" height="212" border="0">
+<tr>
+<td colspan="3"><div align="center">CHỨC NĂNG THÊM MỚI CHUYÊN MỤC</div></td>
+</tr>
+<tr>
+<td width="211">Tên Chuyên Mục :</td>
+<td colspan="2"><input type="text" name="tenchuyenmuc" id="tenchuyenmuc"></td>
+</tr>
+<tr>
+<td>Loại Tin</td>
+<?php
+    $sql= "select * from loaitin";
+    $loaitin=mysqli_query($sqli,$sql);
+    ?>
+<td colspan="2"><select name="loaitin" id="loaitin">
+<?php
+    while($dong_loaitin = mysqli_fetch_array($loaitin))
+    {
+        ?>
+<option value="<?php echo $dong_loaitin["idloaitin"]?>"> <?php echo $dong_loaitin["tenloaitin"]?> </option>
+
+<?php
+    }
+    ?>
+
+</select></td>
+</tr>
+<tr>
+<td>Thứ tự:</td>
+<td colspan="2"><label for="thutu"></label>
+<input type="text" name="thutu" id="thutu"></td>
+</tr>
+<tr>
+<td>Trạng thái:</td>
+<td colspan="2"><select name="trangthai" id="trangthai">
+<option value="Hiển thị">Hiển thị</option>
+<option value="Không hiển thị">Không hiển thị</option>
+</select></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td width="54"><input type="submit" name="them" id="them" value="Thêm"></td>
+<td width="144"><input type="submit" name="reset" id="reset" value="Reset"></td>
+</tr>
+</table>
+</div>
+</form>
+
+
 
 //CuongDao - version 1.1 - thêm chức năng sửa bài viết
 <?php
